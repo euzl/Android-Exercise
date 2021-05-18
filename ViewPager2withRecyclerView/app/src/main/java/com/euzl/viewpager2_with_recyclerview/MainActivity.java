3bb5bea18d81913f614d558f4197e738356133aa
euzl <euzl@khu.ac.kr>
2021-05-19 02:23:28 +0900

    기존 ViewPager2 코드 불러오기

---
+++ ViewPager2withRecyclerView/app/src/main/java/com/euzl/viewpager2_with_recyclerview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.euzl.viewpager2_with_recyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    PagerAdapter pagerAdapter;
+    ViewPager2 viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pagerAdapter = new PagerAdapter(this);
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(pagerAdapter);
+
+    }
+
+    public class PagerAdapter extends FragmentStateAdapter {
+
+        public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            Fragment fragment = new PageFragment();
+            Bundle args = new Bundle();
+            args.putInt(PageFragment.ARG_OBJECT, position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+    }
+}
