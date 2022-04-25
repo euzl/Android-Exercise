56909c32691e045a142b47d66644edbdb52d57b9
yujinlee <yujinlee@gmarket.com>
2022-04-25 10:39:46 +0900

    configuration activity 적용

--- AppWidget/app/src/main/java/com/euzl/appwidget/AppWidgetConfigure.kt
+++ AppWidget/app/src/main/java/com/euzl/appwidget/AppWidgetConfigure.kt
@@ -1,11 +1,62 @@
 package com.euzl.appwidget
 
+import android.app.Activity
+import android.appwidget.AppWidgetManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.euzl.appwidget.databinding.ActivityAppWidgetConfigureBinding
 
-class AppWidgetConfigure : AppCompatActivity() {
+class AppWidgetConfigure() : AppCompatActivity() {
+
+    lateinit var binding: ActivityAppWidgetConfigureBinding
+    var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_app_widget_configure)
+
+        // result := CANCELED
+        // Back button 을 누르는 등 호스트가 위젯을 놓다가 취소했을 때
+        setResult(RESULT_CANCELED)
+
+
+        // set Data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_app_widget_configure)
+        binding.activity = this
+
+        // intent로부터 Widget Id 얻기
+        appWidgetId = intent?.extras?.getInt(
+            AppWidgetManager.EXTRA_APPWIDGET_ID,
+            appWidgetId
+        ) ?: appWidgetId
+
+        // widget id 가 없이 오면 끝
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+        }
+
+        // remote view를 통해 레이아웃 업데이트
+//        RemoteView(packageName, R.layout.example_appwidget).also { views ->
+//            appWidgetManager.updateAppWidget(appWidgetId, views)
+//        }
+
+
+    }
+
+    fun onConfirmClick(view: View) {
+        val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(this)
+        // ExampleAppWidgetProvider do something
+
+
+        // app widget id 를 포함한 결과 반환
+        val resultValue = Intent().apply {
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        }
+        setResult(Activity.RESULT_OK, resultValue)
+        finish()
     }
 }
