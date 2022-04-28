d8f1df6c1e88fbd45691428603708a45b2ddfb7e
yujinlee <yujinlee@gmarket.com>
2022-04-28 13:45:45 +0900

    유저 배경화면 불러와서 미리보기 표시
    - wallpaperManager
    - request permission

--- AppWidget/app/src/main/java/com/euzl/appwidget/AppWidgetConfigure.kt
+++ AppWidget/app/src/main/java/com/euzl/appwidget/AppWidgetConfigure.kt
@@ -1,8 +1,11 @@
 package com.euzl.appwidget
 
+import android.Manifest
 import android.app.Activity
+import android.app.WallpaperManager
 import android.appwidget.AppWidgetManager
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +15,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RemoteViews
 import android.widget.SeekBar
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
 import androidx.databinding.DataBindingUtil
@@ -19,8 +23,8 @@ import com.euzl.appwidget.databinding.ActivityAppWidgetConfigureBinding
 
 class AppWidgetConfigure() : AppCompatActivity() {
 
-    lateinit var binding: ActivityAppWidgetConfigureBinding
-    var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
+    private lateinit var binding: ActivityAppWidgetConfigureBinding
+    private var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
     lateinit var ivBackground: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +39,18 @@ class AppWidgetConfigure() : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_app_widget_configure)
         binding.activity = this
 
+        // user wall paper (퍼미션 확인 후)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            binding.ivWallPaper.setImageDrawable(WallpaperManager.getInstance(applicationContext).drawable)
+        }
+
         // background image for opacity
         ivBackground = findViewById<ImageView?>(R.id.ivWidgetBackground).apply {
-            setColorFilter(R.color.black)
+            setColorFilter(R.color.white)
         }
 
         // intent로부터 Widget Id 얻기
@@ -55,7 +68,6 @@ class AppWidgetConfigure() : AppCompatActivity() {
     fun onSeekBarProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
         binding.textView.text = "배경 투명도 조절 : ${progress*100/255}%"
         ivBackground.imageAlpha = binding.seekBar.progress
-//        ivBackground.setColorFilter(ContextCompat.getColor(applicationContext, R.color.black))
     }
 
     fun onConfirmClick(view: View) {
@@ -64,7 +76,7 @@ class AppWidgetConfigure() : AppCompatActivity() {
         // remote view를 통해 레이아웃 업데이트
         RemoteViews(packageName, R.layout.example_appwidget).apply {
             setTextViewText(R.id.textView, "투명도 ${binding.seekBar.progress*100/255}%")
-            setInt(R.id.ivWidgetBackground, "setColorFilter", R.color.black)
+            setInt(R.id.ivWidgetBackground, "setColorFilter", R.color.white)
             setInt(R.id.ivWidgetBackground, "setImageAlpha", binding.seekBar.progress)
             appWidgetManager.updateAppWidget(appWidgetId, this)
         }
